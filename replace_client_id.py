d99220c0f402b7040edc35540b73cfe87aad81cd
JeonJongYook <jggen0121@koolsign.net>
2026-09-20 22:39:47 +0900

    설정: 임시 스크립트(.py, .js) 파일 git 추적 제외 및 .gitignore 추가

--- replace_client_id.py
+++
@@ -1,7 +0,0 @@
-with open('src/app/layout.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace('dxr1zrae95', 'f9sp6e02ix')
-
-with open('src/app/layout.tsx', 'w') as f:
-    f.write(content)
--- test.js
+++
@@ -1,2 +0,0 @@
-const html = `<h5><div></div></h5>`;
-console.log("This is technically invalid HTML5");
